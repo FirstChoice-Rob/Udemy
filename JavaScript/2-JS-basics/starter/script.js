310c2ffa2d9741f562b41816b408d6288aa8c8ce
Higrob2004 <40383128+Higrob2004@users.noreply.github.com>
2018-06-29 22:28:32 +0100

    Fix of merge

--- JavaScript/2-JS-basics/starter/script.js
+++ JavaScript/2-JS-basics/starter/script.js
@@ -1,3 +1,21 @@
+Skip to content
+ 
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ @FirstChoice-Rob Sign out
+0
+0 0 FirstChoice-Rob/Udemy
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki  Insights  Settings
+Udemy/JavaScript/2-JS-basics/starter/script.js
+027568c  7 minutes ago
+@FirstChoice-Rob FirstChoice-Rob Merge branch 'master' of https://github.com/Higrob2004/Udemy
+@FirstChoice-Rob @RobOnline
+     
+336 lines (245 sloc)  6.56 KB
 /* 
 Variables and data types
 */
@@ -8,7 +26,6 @@ var lastName = 'Walker';
 var age= 28;
 var fullAge = true;
 var job;
-
 job = 'Teacher';
 console.log(firstName, lastName,age,fullAge,job);
 */
@@ -22,25 +39,16 @@ Variable Mutaions and type coercion
 // Type Coercion
 var firstName = 'Robert';
 var age = 38;
-
 console.log(firstName +' ' + age);
-
 var job,isMarried;
 job = 'Teacher';
 isMarried = false;
-
 console.log(firstName +' is a '+ age +' year old ' + job +'. Is he single? ' + isMarried );
-
-
 // Variable mutaion
 age = 'Thirty Eight';
 job = 'Developer';
 console.log(age);
-
 alert(firstName +' is a '+ age +' year old' + job +'. Is he single? ' + isMarried );
-
-
-
 var lastName =prompt('What is his last name?');
 console.log(firstName,lastName);*/
 
@@ -60,58 +68,42 @@ console.log(yearBirth);
 console.log(now + 2);
 console.log(now / 2);
 console.log(now * 2);
-
-
 // Logical Operator
-
 var ageJohn, ageMark;
 ageJohn = 28;
 ageMark = 40;
-
 var johnOlder = ageJohn > ageMark;
 var markOlder = ageJohn < ageMark;
 console.log(johnOlder);
 console.log(markOlder);
-
-
 //typeof
 console.log(typeof johnOlder);
 console.log(typeof now);
 console.log(typeof currentTime);
 console.log(typeof 'My Name Is Rob');
-
 */
 
 /*************************
  * Operator Precedence 
  * 
-
  //multiple operators
 var age = 38;
 var currentTime = new Date();
 var now = currentTime.getFullYear();
 var yearRob = now - age;
 var fullage = 18;
-
 var isFullage = now - yearRob >= fullage;
-
 console.log(isFullage);
-
 // grouping
 var ageRob = now -yearRob;
 var ageJames = 42;
 var averageAge = (ageJames + ageRob) /2
 console.log(averageAge);
-
-
 //multiple assignments
 var x,y;
 x = y = (3+5) *4 - 6; //8*4-6 //32 -6 // 26
 console.log(x,y);
-
-
 //more operators
-
 //x = x *2;
 x*=2;
 console.log(x,y);
@@ -131,14 +123,11 @@ var personOneName = prompt("What is the First Persons Name?")
 var personOneHeight = prompt("What is " + personOneName +'s Height in meters')
 var personOneKG = prompt("What is " + personOneName +'s Weight in KG')
 var PersonOneBMI = personOneKG / (personOneHeight * personOneHeight)
-
 var personTwoName = prompt("What is the Second Persons Name?")
 var personTwoHeight = prompt("What is " + personTwoName +'s Height in meters')
 var personTwoKG = prompt("What is " + personTwoName +'s Weight in KG')
 var PersonTwoBMI = personTwoKG / (personTwoHeight * personTwoHeight)
-
 var highestBMi = Math.max(PersonOneBMI,PersonTwoBMI)
-
 if (highestBMi = PersonOneBMI)
 {   
     alert(personOneName +' has a higher BMI ('+ PersonOneBMI +') than ' + personTwoName +"'s BMI("+ PersonTwoBMI +')');
@@ -147,6 +136,121 @@ else
 {   
     alert(personTwoName +' has a higher BMI ('+ PersonTwoBMI +') than ' + personOneName +"'s BMI("+ PersonOneBMI +')');
 }*/
+
+
+
+
+
+
+
+/**************************
+ If/ Else Ststements
+ 
+ var firstName = 'John';
+ var civilStatus = 'Single';
+ if (civilStatus === 'Married')
+ {
+    console.log(firstName + ' is married');
+ }
+ else
+ {
+console.log(firstName + ' is not married');
+ }
+ var isMarried = true
+ if (isMarried)
+ {
+    console.log(firstName + ' is married');
+ }
+ else
+ {
+console.log(firstName + ' is not married');
+ }*/
+
+
+ /**************************
+  Boolean Logic
+  */
+/*
+  var firstName = 'John';
+  var age = 20
+  if (age <13)
+  {
+      console.log(firstName + ' is a boy');      
+  }
+  else if(age >= 13 && age < 20)
+  {
+    console.log(firstName + ' is a teenage');
+  } 
+  else if(age >=20 && age <30)
+  {
+    console.log(firstName + ' is a young man');
+  }
+  else
+  {
+      console.log(firstName + ' is a man');
+  }*/
+
+
+
+  /********************************** 
+   * The Ternary Operator and switch statements
+  
+
+  var firstName = 'John';
+  var age = 21;
+
+  //Ternary
+  age >= 18 ? console.log(firstName + ' drinks beers')
+  :console.log(firstName + ' drinks juice');
+
+  var drink = age >= 18 ? 'Beer' : 'Juice';
+
+  console.log(firstName + ' drinks ' + drink );
+
+
+  var job ='Instructor';
+  //Switch
+  switch(job)
+  {
+      case 'Teacher':
+      case 'Instructor':
+      console.log(firstName + ' teaches kids how to code.');
+      break;
+
+      case 'driver':
+      console.log(firstName + ' drive a uber in Berlin.');
+      break;
+
+      case 'diver':
+      console.log(firstName + ' dives in Thailand.');
+      break;
+
+      default:
+      console.log(firstname + 'has an undefinded job');
+
+  }
+
+  switch(true)
+  {
+        case age <13 :
+        console.log(firstName + ' is a boy');
+        break;
+
+        case age >=13 && age < 20:
+        console.log(firstName + ' is a teenager'); 
+        break;
+
+        case age >= 20 && age <30:
+        console.log(firstName + ' is a young man');  
+        break;
+
+        default:
+        console.log(firstName + ' is a man'); 
+  }
+
+*/
+
+
 
 
 // Truthy and falsy values & equality operators
@@ -162,16 +266,11 @@ if (height){
     else{
         console.log("variable is not definded");
     }
-
-
-
     // Equality Variable
-
     if (height == '23')
     {
         console.log("The == operator does type coercion!");
     }
-
     if (height === '23')
     {
         console.log("The === operator does type coercion!");
@@ -181,9 +280,7 @@ if (height){
         console.log("The === operator does NOT DO type coercion!");
     }*/
 
-
-
-
+  
     // code Challenge 2
 
  var teamOneScore = new Array();
